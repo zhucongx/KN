c515c694ed00ce451bd90f37a20de18a6af7db04
zhucongx <xizhucong@gmail.com>
2020-02-04 18:03:09 -0500

    update

--- include/Configuration.h
+++ include/Configuration.h
@@ -5,15 +5,19 @@
 #ifndef _CONFIGURATION_H_
 #define _CONFIGURATION_H_
 
-
 #include <string>
 #include <fstream>
+#include <iostream>
 #include <array>
 #include <vector>
 #include "Atom.h"
 
 using std::array;
 using std::vector;
+using std::ifstream;
+
+enum { XX = 0, YY = 1, ZZ = 2, XY = 3, YZ = 4, ZX = 5 };
+enum { X = 0, Y = 1, Z = 2 };
 
 class Configuration {
  private:
@@ -24,6 +28,7 @@ class Configuration {
   array<double, 9> cell;
   // length of three edges
   array<double, 3> length;
+  // bvx, bvy, bvz form a matrix matching H0 in .cfg file
   array<double, 3> bvx, tvx, bvy, tvy, bvz, tvz;
   vector<Atom> atoms;
   vector<int> vacList;
@@ -35,10 +40,9 @@ class Configuration {
   bool operator<=(const Configuration &rhs) const;
   bool operator>=(const Configuration &rhs) const;
 
-  void readLammpsData(const string & fileName);
-  void readConfig(const string & fileName);
-  void readPOSCAR(const string & fileName);
-
+  void readLammpsData(const string &fileName);
+  bool readConfig(const string &fileName);
+  void readPOSCAR(const string &fileName);
 };
 
 #endif //KN_SRC_CONFIGURATION_H_
--- src/Configuration.cpp
+++ src/Configuration.cpp
@@ -19,6 +19,38 @@ bool Configuration::operator>=(const Configuration &rhs) const {
   return !(*this < rhs);
 }
 void Configuration::readLammpsData(const string &fileName) {
-
+}
+bool Configuration::readConfig(const string &fileName) {
+  ifstream ifs(fileName, ifstream::in);
+  if (ifs.fail()) {
+    std::cout << "Error when open file." << std::endl;
+    return false;
+  }
+  string buff;
+  std::getline(ifs, buff);
+  sscanf(buff.c_str(), "Number of particles = %i", &numAtoms);
+  std::getline(ifs, buff); // A = 1.0 Angstrom (basic length-scale)
+  std::getline(ifs, buff);
+  sscanf(buff.c_str(), "H0(1,1) = %lf A", &bvx[X]);
+  std::getline(ifs, buff);
+  sscanf(buff.c_str(), "H0(1,2) = %lf A", &bvx[Y]);
+  std::getline(ifs, buff);
+  sscanf(buff.c_str(), "H0(1,3) = %lf A", &bvx[Z]);
+  std::getline(ifs, buff);
+  sscanf(buff.c_str(), "H0(2,1) = %lf A", &bvy[X]);
+  std::getline(ifs, buff);
+  sscanf(buff.c_str(), "H0(2,2) = %lf A", &bvy[Y]);
+  std::getline(ifs, buff);
+  sscanf(buff.c_str(), "H0(2,3) = %lf A", &bvy[Z]);
+  std::getline(ifs, buff);
+  sscanf(buff.c_str(), "H0(3,1) = %lf A", &bvz[X]);
+  std::getline(ifs, buff);
+  sscanf(buff.c_str(), "H0(3,2) = %lf A", &bvz[Y]);
+  std::getline(ifs, buff);
+  sscanf(buff.c_str(), "H0(3,3) = %lf A", &bvz[Z]);
+  std::getline(ifs, buff); // .NO_VELOCITY.
+  std::getline(ifs, buff);
+  int entry = 3;
+  sscanf(buff.c_str(), "entry_count = %i", &entry);
 }
 
