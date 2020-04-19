f688a2c2d7820ce168007cdced2303481ea42045
zhucongx <zhucongx@umich.edu>
2020-04-19 03:52:29 -0400

    cmake

--- cmake/FindARMADILLO.cmake
+++ cmake/FindARMADILLO.cmake
@@ -1,10 +1,4 @@
-if (APPLE)
-    message("-- Looking for armadillo on macOS")
-    set(FIND_ARMADILLO_PATHS /usr/ /usr/local)
-elseif (UNIX)
-    message("-- Looking for armadillo on Linux")
-    set(FIND_ARMADILLO_PATHS ~/armadillo/ /usr/)
-endif()
+set(FIND_ARMADILLO_PATHS ~/armadillo/ /usr/ /usr/local)
 
 find_path(ARMADILLO_INCLUDE_DIRS
         NAMES armadillo
@@ -12,6 +6,4 @@ find_path(ARMADILLO_INCLUDE_DIRS
         PATHS ${FIND_ARMADILLO_PATHS})
 find_library(ARMADILLO_LIBRARIES
         NAMES armadillo
-        PATH_SUFFIXES lib lib64
         HINTS ${FIND_ARMADILLO_PATHS} ENV var)
-message(${ARMADILLO_LIBRARIES})
