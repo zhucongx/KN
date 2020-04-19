7870f8aa92c8cba623b3075f69f7338057d2ec28
zhucongx <zhucongx@umich.edu>
2020-04-19 00:56:18 -0400

    Create armadillo-config.cmake

---
+++ cmake/armadillo-config.cmake
@@ -0,0 +1,15 @@
+if (APPLE)
+    message("-- Looking for armadillo on macOS")
+    set(FIND_ARMADILLO_PATHS /usr/local/)
+elseif (UNIX)
+    message("-- Looking for armadillo on Linux")
+    set(FIND_ARMADILLO_PATHS /usr/ ~/armadillo)
+endif()
+
+find_path(ARMADILLO_INCLUDE_DIRS armadillo
+        PATH_SUFFIXES include
+        PATHS ${FIND_ARMADILLO_PATHS})
+find_library(ARMADILLO_LIBRARIES
+        NAMES libarmadillo
+        PATH_SUFFIXES lib64
+        PATHS ${FIND_ARMADILLO_PATHS})
