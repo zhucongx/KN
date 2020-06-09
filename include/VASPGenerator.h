2f01e3fd7159bfc54257f9ceced3ee68017fba3d
zhucongx <zhucongx@umich.edu>
2020-06-09 15:46:08 -0400

    prepare vasp files

---
+++ include/VASPGenerator.h
@@ -0,0 +1,15 @@
+#ifndef KN_INCLUDE_VASPGENERATOR_H_
+#define KN_INCLUDE_VASPGENERATOR_H_
+#include <string>
+#include <set>
+#include "VectorMatrix.h"
+
+namespace kn {
+class VASPGenerator {
+  public:
+
+};
+
+}// namespace kn
+
+#endif //KN_INCLUDE_VASPGENERATOR_H_
