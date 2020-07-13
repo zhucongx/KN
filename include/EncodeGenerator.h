ebe6ff5e8a45024940504838aa6c19688d984d6f
zhucongx <zhucongx@umich.edu>
2020-07-13 01:04:14 -0400

    fix

--- include/EncodeGenerator.h
+++ include/EncodeGenerator.h
@@ -7,12 +7,9 @@
 #include "Config.h"
 
 namespace kn :: EncodeGenerator {
-// public:
 void PrintOutEncode(const std::string &reference_filename);
 std::vector<std::array<std::string, Al_const::kLengthOfEncodes>> Encode(
     const Config &config,
     const std::pair<int, int> &jump_pair);
-
-};
 } // namespace kn
 #endif //KN_INCLUDE_ENCODEGENERATOR_H_
