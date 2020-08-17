544ce0ed08999c3247074b423efdace3bad8c2f9
zhucongx <zhucongx@umich.edu>
2020-08-17 14:50:52 -0400

    make bond class hashable

--- include/Bond.h
+++ include/Bond.h
@@ -6,11 +6,25 @@ namespace kn {
 class Bond {
   public:
     Bond(std::string type1, std::string type2);
-    bool operator<(const Bond &rhs) const;
+    [[nodiscard]] const std::string &GetType1() const;
+    [[nodiscard]] const std::string &GetType2() const;
     friend std::ostream &operator<<(std::ostream &os, const Bond &bond);
+    friend bool operator<(const Bond &lhs, const Bond &rhs);
+    friend bool operator==(const Bond &lhs, const Bond &rhs);
+    friend bool operator!=(const Bond &lhs, const Bond &rhs);
   private:
     std::string type1_;
     std::string type2_;
 };
 } // namespace kn
+
+namespace std {
+template<>
+struct hash<kn::Bond> {
+  std::size_t operator()(const kn::Bond &k) const {
+    return (std::hash<std::string>()(k.GetType1())) ^ (std::hash<std::string>()(k.GetType2()));
+  }
+};
+}
+
 #endif //KN_SRC_BOND_H_
