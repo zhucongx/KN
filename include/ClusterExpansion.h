ec3619e0e050b4aecc5b89f25e3ef7897ddd1c03
zhucongx <zhucongx@umich.edu>
2020-08-27 17:57:07 -0400

    update

--- include/ClusterExpansion.h
+++ include/ClusterExpansion.h
@@ -6,8 +6,12 @@
 
 namespace kn::ClusterExpansion {
 // initial and final state
+// this function rotate and sort the config in a particular way, basically from center to outside
+Config GetStableStateConfig(const Config & config);
 
 // transition state
+Config GetTransitionStateConfig(const Config & config);
+
 } // namespace kn::ClusterExpansion
 
 #endif //KN_INCLUDE_CLUSTEREXPANSION_H_
