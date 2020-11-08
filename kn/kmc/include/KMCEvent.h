b771598ecedb3eb17e938a5734c3018bcbc3d3f4
Zhucong Xi <zhucongx@umich.edu>
2020-11-08 01:36:29 -0500

    style

--- kn/kmc/include/KMCEvent.h
+++ kn/kmc/include/KMCEvent.h
@@ -2,22 +2,25 @@
 #define KN_KN_KMC_INCLUDE_KMCEVENT_H_
 #include <utility>
 
-namespace kn {
+namespace kmc {
 class KMCEvent {
   public:
-
+    /// Constructor
     explicit KMCEvent(std::pair<size_t, size_t> jump_pair);
-
+    /// Getter
     [[nodiscard]] const std::pair<size_t, size_t> &GetJumpPair() const;
     [[nodiscard]] double GetBarrier() const;
-    void SetBarrier(double barrier);
     [[nodiscard]] double GetRate() const;
-    void SetRate(double rate);
     [[nodiscard]] double GetEnergyChange() const;
-    void SetEnergyChange(double energy_change);
     [[nodiscard]] double GetProbability() const;
-    void SetProbability(double probability);
     [[nodiscard]] double GetCumulativeProvability() const;
+
+    /// Setter
+    void SetJumpPair(const std::pair<size_t, size_t> &jump_pair);
+    void SetBarrier(double barrier);
+    void SetRate(double rate);
+    void SetEnergyChange(double energy_change);
+    void SetProbability(double probability);
     void SetCumulativeProvability(double cumulative_provability);
 
   protected:
@@ -28,6 +31,6 @@ class KMCEvent {
     double probability_;
     double cumulative_provability_;
 };
-} // namespace kn
+} // namespace kmc
 
 #endif //KN_KN_KMC_INCLUDE_KMCEVENT_H_
--- kn/kmc/src/KMCEvent.cpp
+++ kn/kmc/src/KMCEvent.cpp
@@ -1,7 +1,7 @@
 #include <utility>
 #include "KMCEvent.h"
 
-namespace kn {
+namespace kmc {
 KMCEvent::KMCEvent(std::pair<size_t, size_t> jump_pair) : jump_pair_(std::move(jump_pair)) {
 }
 
@@ -13,39 +13,43 @@ double KMCEvent::GetBarrier() const {
   return barrier_;
 }
 
-void KMCEvent::SetBarrier(double barrier) {
-  barrier_ = barrier;
-}
-
 double KMCEvent::GetRate() const {
   return rate_;
-}
-
-void KMCEvent::SetRate(double rate) {
-  rate_ = rate;
 }
 
 double KMCEvent::GetEnergyChange() const {
   return energy_change_;
 }
 
-void KMCEvent::SetEnergyChange(double energy_change) {
-  energy_change_ = energy_change;
-}
-
 double KMCEvent::GetProbability() const {
   return probability_;
-}
-
-void KMCEvent::SetProbability(double probability) {
-  probability_ = probability;
 }
 
 double KMCEvent::GetCumulativeProvability() const {
   return cumulative_provability_;
 }
 
+void KMCEvent::SetJumpPair(const std::pair<size_t, size_t> &jump_pair) {
+  jump_pair_ = jump_pair;
+}
+
+void KMCEvent::SetBarrier(double barrier) {
+  barrier_ = barrier;
+}
+
+void KMCEvent::SetRate(double rate) {
+  rate_ = rate;
+}
+
+void KMCEvent::SetEnergyChange(double energy_change) {
+  energy_change_ = energy_change;
+}
+
+void KMCEvent::SetProbability(double probability) {
+  probability_ = probability;
+}
+
 void KMCEvent::SetCumulativeProvability(double cumulative_provability) {
   cumulative_provability_ = cumulative_provability;
 }
-} // namespace kn
+} // namespace kmc
