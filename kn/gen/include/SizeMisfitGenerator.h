b1dd8c89cba3b0d4804af5343856d7f62283c9e7
Zhucong Xi <xizhucong@gmail.com>
2020-11-23 14:57:12 -0500

    override

--- kn/gen/include/SizeMisfitGenerator.h
+++ kn/gen/include/SizeMisfitGenerator.h
@@ -10,7 +10,7 @@ class SizeMisfitGenerator : public ConfigGenerator {
                         const std::string &solvent_element,
                         const std::set<std::string> &element_list,
                         const std::filesystem::path &pot_folder_path);
-    virtual ~SizeMisfitGenerator();
+    ~SizeMisfitGenerator() override;
     void CreateConfigs() const override;
   private:
 };
