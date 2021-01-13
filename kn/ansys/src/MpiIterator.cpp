222d8558e46d8848a6ce84ce53301c10224e7429
Zhucong Xi <zhucongx@umich.edu>
2021-01-13 00:24:48 -0500

    SerialRun find_cluster

--- kn/ansys/src/MpiIterator.cpp
+++ kn/ansys/src/MpiIterator.cpp
@@ -1,4 +1,6 @@
 #include "MpiIterator.h"
+#include <iostream>
+#include <mpi.h>
 
 namespace ansys {
 
@@ -7,14 +9,16 @@ MpiIterator::MpiIterator(unsigned long long int initial_number,
                          unsigned long long int finial_number)
     : initial_number_(initial_number),
       increment_number_(increment_number),
-      finial_number_(finial_number),
-      mpi_rank_(world_.rank()),
-      mpi_size_(world_.size()) {
+      finial_number_(finial_number) {
+  MPI_Init(nullptr, nullptr);
+  MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank_);
+  MPI_Comm_size(MPI_COMM_WORLD, &mpi_size_);
   if (mpi_rank_ == 0) {
     std::cout << "Using " << mpi_size_ << " processes." <<
               std::endl;
   }
 }
-MpiIterator::~MpiIterator() = default;
-
+MpiIterator::~MpiIterator() {
+  MPI_Finalize();
+}
 } // namespace ansys
