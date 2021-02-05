912a17527cfcaffe41347a9065f16060745aa1d4
Zhucong Xi <zhucongx@umich.edu>
2021-02-05 01:37:18 -0500

    add constants

--- kn/cfg/include/Constants.hpp
+++ kn/cfg/include/Constants.hpp
@@ -11,6 +11,9 @@ constexpr double kSecondNearestNeighborsCutoff = 4.5;
 constexpr double kThirdNearestNeighborsCutoff = 5.3;
 // length between fourth nearest neighbors is double of length of first nearest neighbors
 constexpr double kFourthNearestNeighborsCutoff = 5.9;
+constexpr double kFifthNearestNeighborsCutoff = 6.5;
+constexpr double kSixthNearestNeighborsCutoff = 7.1;
+constexpr double kSeventhNearestNeighborsCutoff = 7.6;
 // constexpr double kNearNeighborsCutoff = kThirdNearestNeighborsCutoff;
 
 
@@ -23,6 +26,13 @@ constexpr size_t kNumSecondNearestNeighbors = 6;
 constexpr size_t kNumThirdNearestNeighbors = 24;
 // For FCC the fourth nearest neighbor is 12
 constexpr size_t kNumFourthNearestNeighbors = 12;
+// For FCC the fifth nearest neighbor is 24
+constexpr size_t kNumFifthNearestNeighbors = 24;
+// For FCC the sixth nearest neighbor is 12
+constexpr size_t kNumSixthNearestNeighbors = 8;
+// For FCC the seventh nearest neighbor is 48
+constexpr size_t kNumSeventhNearestNeighbors = 48;
+
 } // namespace Al_const
 #endif //KN_KN_CFG_INCLUDE_CONSTANTS_HPP_
 
